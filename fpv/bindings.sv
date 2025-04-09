ba3526e6707e810146b92818fd4d530efebd9422
Anderson Domingues <ti.andersondomingues@gmail.com>
2025-04-09 14:06:03 -0300

    add fpv property for mem writing op @ DataMemory

--- fpv/bindings.sv
+++ fpv/bindings.sv
@@ -3,17 +3,17 @@
 module dataMemory_bind_top;
 
 	bind DataMemory dataMemory_prop dataMemory_bind(
-	  .A(A),                        .WD(WD),
-	  .out(out),      .mem_write(mem_write),
-	  .clk(clk),
-      .reset(reset),
-	  //outs >> d100, green, atum, bacon, busy;
-
+    .clk(clk),
+    .reset(reset),
+    
+    // binds interface
+	  .A(A),
+    .WD(WD),
+	  .out(out),
+    .mem_write(mem_write),
+	  
 	  //internals
-      .register(register)
-	  //.count(count),
-	  //.pe(pe),
-	  //.ea(ea)
+    .register(register)
 	);
 
 endmodule
--- fpv/fpv.tcl
+++ fpv/fpv.tcl
@@ -2,8 +2,6 @@
 
 clear -all
 
-# analyze the design
-# analyze -vhdl ../../../rtl/v4/t4.vhd ;
 analyze -sv ../rtl/8_Bit_Processor/Sources/DataMemory.sv ;
 
 # Analyze property files
@@ -16,26 +14,15 @@ elaborate -top {DataMemory}
 
 # Set up Clocks and Resets
 clock clk -factor 1 -phase 1
-reset -expression {reset = '1'};
+reset -expression {reset == 1'b1};
 
 # get designs statistics
-get_design_info
-#Statistics [for instance "sanduiche"]
-#---------------------------
-# Flops:         2 (10) (0 property flop bits)
-# Latches:       0 (0)
-# Gates:         80 (501)
-# Nets:          95
-# Ports:         12
-# RTL Lines:     113
-# RTL Instances: 1
-# Embedded Assumptions: 0
-# Embedded Assertions:  0
-# Embedded Covers:      0
+set_max_trace_length 150
+get_design_info -list multiple_driven
 
-# this command might be useful for more complex designs
-#set_max_trace_length 150
 prove -all
 
 # Report proof results
 report
+
+# check_loop -viewer
